1a32cd4d6b911fe3625e8a048aba0db96e16dd5c
Buqing Fan <fanbuqing111@gmail.com>
2026-06-15 12:39:15 +0800

    Add files via upload

---
+++ mmseg/datasets/fengxi.py
@@ -0,0 +1,28 @@
+from mmseg.registry import DATASETS
+from .basesegdataset import BaseSegDataset
+
+
+@DATASETS.register_module()
+class fengxiDataset(BaseSegDataset):
+    """gas Potsdam dataset.
+
+     In segmentation map annotation for Potsdam dataset, 0 is the ignore index.
+     ``reduce_zero_label`` should be set to True. The ``img_suffix`` and
+     ``seg_map_suffix`` are both fixed to '.png'.
+     """
+    METAINFO = dict(
+        classes=['background', 'fengxi'],  # 显式添加背景类别
+        palette=[[255, 255, 255], [0, 0, 0]])  # 背景为白色，气体为黑色（reduce_zero_label=False）
+
+
+
+    def __init__(self,
+                 img_suffix='.jpg',
+                 seg_map_suffix='.png',
+                 reduce_zero_label=False,
+                 **kwargs) -> None:
+        super().__init__(
+            img_suffix=img_suffix,
+            seg_map_suffix=seg_map_suffix,
+            reduce_zero_label=reduce_zero_label,
+            **kwargs)
---
+++ mmseg/datasets/gas1.py
@@ -0,0 +1,31 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmseg.registry import DATASETS
+from .basesegdataset import BaseSegDataset
+
+
+@DATASETS.register_module()
+class gasDataset(BaseSegDataset):
+    """gas Potsdam dataset.
+
+     In segmentation map annotation for Potsdam dataset, 0 is the ignore index.
+     ``reduce_zero_label`` should be set to True. The ``img_suffix`` and
+     ``seg_map_suffix`` are both fixed to '.png'.
+     """
+    METAINFO = dict(
+        classes=['background', 'gas'],  # 显式添加背景类别
+        palette=[[0, 0, 0], [255, 255, 255]])  # 背景为黑色，气体为白色（reduce_zero_label=False）
+
+#    METAINFO = dict(
+#        classes=['gas'],
+#        palette=[[255, 255, 255]])
+
+    def __init__(self,
+                 img_suffix='.jpg',
+                 seg_map_suffix='.png',
+                 reduce_zero_label=False,
+                 **kwargs) -> None:
+        super().__init__(
+            img_suffix=img_suffix,
+            seg_map_suffix=seg_map_suffix,
+            reduce_zero_label=reduce_zero_label,
+            **kwargs)
